47aa96bca2876b0b3139ab28f2c024765238728a
fuadcodez <fuadabolaji1@gmail.com>
2024-10-12 16:44:32 +0100

    create the about page

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+
+
+export default function AboutPage() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eligendi expedita praesentium! Illo voluptatem nam repudiandae expedita assumenda. Vitae veritatis, suscipit tenetur aperiam optio molestiae expedita necessitatibus, ea culpa facere repudiandae nisi cum? Qui tempore ratione modi temporibus sapiente sint assumenda porro iste animi cupiditate obcaecati, voluptates mollitia recusandae tempora repellendus hic inventore ut, atque expedita nesciunt eaque! Suscipit dolorum,
+        </p>
+        <p>reprehenderit aliquam cum quod minus nihil ipsum possimus illum aliquid veritatis accusantium, optio temporibus ex amet assumenda aspernatur. Inventore quos beatae laudantium labore sint magnam facere distinctio quidem nesciunt accusantium similique quas velit hic id, vero maxime minus. Ad, cumque.</p>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatem labore odit earum deleniti, non aut rem, eum obcaecati placeat atque tempora harum in nisi suscipit sint dignissimos hic assumenda quasi autem, alias delectus. Sint consequuntur vero maiores similique alias corrupti dolorum, hic sapiente deserunt debitis, exercitationem quas laboriosam architecto quam natus? Magnam illo veniam pariatur eligendi reprehenderit magni earum dolore sapiente deserunt doloremque quasi tempora optio, numquam fuga quae!</p>
+    </div>
+  )
+}
